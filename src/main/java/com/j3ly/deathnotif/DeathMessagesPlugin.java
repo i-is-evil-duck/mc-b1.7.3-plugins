493ca011b997fcefe8fad0724ed313c2f2eb3439
I Is Evil Duck <140123334+i-is-evil-duck@users.noreply.github.com>
2025-07-23 00:01:00 -0700

    Add files via upload

---
+++ src/main/java/com/j3ly/deathnotif/DeathMessagesPlugin.java
@@ -0,0 +1,52 @@
+package com.j3ly.deathnotif;
+
+import java.util.Random;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityListener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.Event;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DeathMessagesPlugin extends JavaPlugin {
+
+    private final Random random = new Random();
+
+    private final String[] messages = {
+        " has died! RIP.",
+        " died LMFAOO",
+        " killed them self",
+        " died in a hole",
+        " jumped",
+        " did a Epstein"
+    };
+
+    private final EntityListener entityListener = new EntityListener() {
+        @Override
+        public void onEntityDeath(EntityDeathEvent event) {
+            if (event.getEntity() instanceof Player) {
+                Player player = (Player) event.getEntity();
+
+                // Pick a random message from the array
+                String randomMessage = messages[random.nextInt(messages.length)];
+
+                String message = player.getName() + randomMessage;
+                getServer().broadcastMessage(message);
+            }
+        }
+    };
+
+    @Override
+    public void onEnable() {
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvent(Event.Type.ENTITY_DEATH, entityListener, Priority.Normal, this);
+        System.out.println("[DeathMessages] Plugin enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("[DeathMessages] Plugin disabled.");
+    }
+}
